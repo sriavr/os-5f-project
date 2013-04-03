3e214a03e2f7a3c9cbb0e2eb010b0fb7cc048ee6
sriavr@gmail.com <sriavr@gmail.com@9a885a25-b30a-b17b-0df4-bc7da42567bc>
2013-04-03 20:08:38 +0000

    changes for pos

--- trunk/src/action/DefaultAction.java
+++ trunk/src/action/DefaultAction.java
@@ -1,7 +1,23 @@
 package action;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class DefaultAction extends ActionSupport {
+
+	public DefaultAction() {
+		Properties properties = new Properties();
+		try {
+			properties.load(Thread.currentThread().getContextClassLoader()
+					.getResourceAsStream("myapp.properties"));
+			String modelDir = properties.getProperty("modelDir");
+			System.out.println("modelDir:" + modelDir);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
